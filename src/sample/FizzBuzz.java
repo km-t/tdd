b00d9b98a956641da17a2bb557fd62d495a1d608
koba <masatennis10140409@gmail.com>
2021-03-09 13:06:51 +0900

    fizzbuzz 模範解答その3完成　テストをパラメータ化した

--- src/sample/FizzBuzz.java
+++ src/sample/FizzBuzz.java
@@ -2,14 +2,17 @@ package sample;
 
 public class FizzBuzz {
 
-    public String convert(int num) {
-        if(num % 3 == 0) {
-            return "Fizz";
+     public String convert(int num) {
+            if (num % 3 == 0 && num % 5 == 0) {
+                return "FizzBuzz";
+            }
+            if (num % 3 == 0) {
+                return "Fizz";
+            }
+            if (num % 5 == 0) {
+                return "Buzz";
+            }
+            return String.valueOf(num);
         }
-        if(num % 5 == 0) {
-            return "Buzz";
-        }
-        return String.valueOf(num);
-    }
 
 }
--- src/sampleTest/MainTest.java
+++ src/sampleTest/MainTest.java
@@ -5,62 +5,57 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import sample.FizzBuzz;
 
-@DisplayName("Fizz Buzz 数列とその変換規則を扱う FizzBuzzクラス")
-public class MainTest {
-
+@DisplayName("Fizz Buzz 数列とその変換規則を扱う FizzBuzz クラス")
+class MainTest {
     private FizzBuzz fizzbuzz;
-
     @BeforeEach
-    private void 準備() {
-        fizzbuzz = new FizzBuzz();
+    void 前準備() {
+         fizzbuzz = new FizzBuzz();
     }
-    /**
-     * 品質保証の観点から考えると、(意識的に考えられた)テストケースが少ない
-     * ex) 境界値分析や同値分割など
-     *
-     */
 
     @Nested
-    class convertメソッドは引数に与えられた整数を文字列に変換する {
-        @Nested
-        class その他の数はそのまま文字列に変換する {
-            @Test
-            void 下の境界値1を渡すと文字列1を返す() throws Exception {
-                assertEquals("1", fizzbuzz.convert(1));
-            }
-            @Test
-            void 下の境界値のひとつ内側の値2を渡すと文字列2を返す() throws Exception {
-                assertEquals("2", fizzbuzz.convert(2));
-            }
+    class convertメソッドは数を文字列に変換する {
+
+        @ParameterizedTest(name = "{1}{0}を渡すと文字列「Fizz」を返すこと")
+        @CsvSource({
+            "3,  '同値類の中の最小の3の倍数'",
+            "99, '上の境界値のひとつ内側の値であり同値類の中の最大の3の倍数'"
+        })
+        void _3の倍数のときは数の代わりにFizzに変換する(int input, String description) {
+            assertEquals("Fizz", fizzbuzz.convert(input));
         }
 
-        @Nested
-        class _3の倍数のときは代わりにFizzを返す {
-            @Test
-            void 同値類の中の最小の3の倍数3を渡すと文字列Fizzを返す() throws Exception {
-                assertEquals("Fizz", fizzbuzz.convert(3));
-            }
-            @Test
-            void 上の境界値のひとつ内側の値であり同値類の中の最大の3の倍数99を渡すと文字列Fizzを返す() throws Exception {
-                assertEquals("Fizz", fizzbuzz.convert(99));
-            }
+        @ParameterizedTest(name = "{1}{0}を渡すと文字列「Buzz」を返すこと")
+        @CsvSource({
+            "5,   '同値類の中の最小の5の倍数'",
+            "100, '上の境界値であり同値類の中の最大の5の倍数'"
+        })
+        void _5の倍数のときは数の代わりにBuzzに変換する(int input, String description) {
+            assertEquals("Buzz", fizzbuzz.convert(input));
         }
 
-        @Nested
-        class _5の倍数のときは代わりにBuzzを返す {
-            @Test
-            void 同値類の中の最小の5の倍数5を渡すと文字列Buzzを返す() throws Exception {
-                assertEquals("Buzz", fizzbuzz.convert(5));
-            }
-            @Test
-            void 上の境界値100を渡すと文字列Buzzを返す() throws Exception {
-                assertEquals("Buzz", fizzbuzz.convert(100));
-            }
+        @ParameterizedTest(name = "{1}{0}を渡すと文字列「FizzBuzz」を返すこと")
+        @CsvSource({
+            "15,  '同値類の中の最小の3と5の公倍数'",
+            "0,   '下の境界値のひとつ外側の値0は3と5両方の倍数でもあるので'"
+        })
+        void _3と5両方の倍数の場合には数の代わりにFizzBuzzに変換する(int input, String description) {
+            assertEquals("FizzBuzz", fizzbuzz.convert(input));
+        }
+
+        @ParameterizedTest(name = "{2}{0}を渡すと文字列「{1}」を返すこと")
+        @CsvSource({
+            "1,   '1',   '下の境界値'",
+            "2,   '2',   '下の境界値のひとつ内側の値'",
+            "101, '101', '下の境界値のひとつ外側の値'"
+        })
+        void その他の数のときはそのまま文字列に変換する(int input, String expected, String description) {
+            assertEquals(expected, fizzbuzz.convert(input));
         }
     }
-
 }
