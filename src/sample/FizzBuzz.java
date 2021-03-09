0ae3ad7ee4262732b914c0a6df68fd917e726217
koba <masatennis10140409@gmail.com>
2021-03-09 11:30:08 +0900

    fizzbuzz 3 done

--- src/sample/FizzBuzz.java
+++ src/sample/FizzBuzz.java
@@ -1,7 +1,11 @@
 package sample;
 
 public class FizzBuzz {
+
     public String convert(int num) {
+        if(num == 3) {
+            return "Fizz";
+        }
         return String.valueOf(num);
     }
 
--- src/sampleTest/MainTest.java
+++ src/sampleTest/MainTest.java
@@ -2,37 +2,38 @@ package sampleTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import sample.FizzBuzz;
 
 public class MainTest {
 
+    private FizzBuzz fizzbuzz;
+
+    @BeforeEach
+    private void 準備() {
+        fizzbuzz = new FizzBuzz();
+        /**
+         * テストメソッドが呼ばれる順番は変わる
+         * →テストの信頼性を確保するため
+         * →テスト間の依存関係が無いということを担保
+         * ex)テスト1で書き込んだ内容をテスト2で読み込むなど
+         */
+    }
+
     @Test
     void _1を渡すと文字列1を返す() throws Exception {
-        //準備
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        //実行&検証
         assertEquals("1", fizzbuzz.convert(1));
-        /*
-         * assertEquals("2", fizzbuzz.convert(2));
-         * を追加しても可能だが、良くない
-         * →テストは失敗したら、そのテストメソッドは中止になる
-         * →どこが正常に動くかわからない＝後進
-         * 　かつ、1つのテストメソッドで何をしたいか分からない
-         *
-         * 1テスト1アサーションを、1 assertion per 1 testという
-         */
-        //片付け(DBの後処理などで、今回はない)
     }
 
     @Test
     void _2を渡すと文字列2を返す() throws Exception {
-        //準備
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        //実行&検証
         assertEquals("2", fizzbuzz.convert(2));
-        //片付け(DBの後処理などで、今回はない)
     }
 
+    @Test
+    void _3を渡すと文字列Fizzを返す() throws Exception {
+        assertEquals("Fizz", fizzbuzz.convert(3));
+    }
 }
